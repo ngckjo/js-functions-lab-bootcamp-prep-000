da8d00d7b03c2054761c8559b98b2bbabd943677
Josh <cheukhinjoshua.ng@gmail.com>
2019-05-30 01:25:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,13 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name){
-  return `Happy holidays, $(name)!`
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidaysTo(holiday,name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(days,holiday){
+  return `It's a $(days) days until ${holiday}!`
 }
